56d962eba24ea198718fa5b75ad90eac6e977284
LeoVC <leovc_lll@outlook.com>
2019-09-03 23:36:10 -0300

    E isso ai

---
+++ Assets/Scripts/Player/MageAreaSkill.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MageAreaSkill : MonoBehaviour
+{
+    List<ParticleCollisionEvent> events = new List<ParticleCollisionEvent>();
+
+    private void OnParticleTrigger()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
