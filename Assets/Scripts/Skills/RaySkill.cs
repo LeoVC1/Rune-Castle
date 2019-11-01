746d3eb1afe8c4b882cc407892ec40e5b450c915
LeoVC <leovc_lll@outlook.com>
2019-11-01 18:47:39 -0300

    É isso.

---
+++ Assets/Scripts/Skills/RaySkill.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaySkill : MonoBehaviour
+{
+    [Header("References:")]
+    public LineRenderer myLineRenderer;
+    public LineRenderer[] otherLineRenderers;
+    public GameObject hitParticle;
+    public GameObject enemyHitParticle;
+
+
+    [Header("Properties:")]
+    public float damageOverTime;
+    public LayerMask layerMask;
+    public LayerMask enemyLayerMask;
+
+    private bool onHit;
+    private RaycastHit hit;
+
+    private void Update()
+    {
+        ActivateRay();
+    }
+
+    private void ActivateRay()
+    {
+        myLineRenderer.SetPosition(0, transform.position);
+        foreach (LineRenderer other in otherLineRenderers)
+        {
+            other.SetPosition(0, transform.position);
+        }
+
+        Ray ray = Camera.main.ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2));
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, enemyLayerMask))
+        {
+            if (hit.collider)
+            {
+                onHit = true;
+                GameObject particle = Instantiate(enemyHitParticle, hit.point, Quaternion.identity);
+                particle.transform.up = hit.normal;
+
+                hit.collider.GetComponent<Enemy>().ReceiveDamage(35);
+            }
+        }
+        else if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
+        {
+            if (hit.collider)
+            {
+                onHit = true;
+                GameObject particle = Instantiate(hitParticle, hit.point, Quaternion.identity);
+                particle.transform.up = hit.normal;
+            }
+        }
+        else
+        {
+            onHit = false;
+        }
+
+        transform.parent.forward = hit.point - transform.position;
+
+        myLineRenderer.SetPosition(1, hit.point);
+        foreach (LineRenderer other in otherLineRenderers)
+        {
+            other.SetPosition(1, hit.point);
+        }
+    }
+
+}
